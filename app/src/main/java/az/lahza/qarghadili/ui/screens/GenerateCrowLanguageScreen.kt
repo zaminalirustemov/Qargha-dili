181359d80fa2334eaa0d4129e742bf3b79bdc8d1
Zaminali <zaminrustemov@bk.ru>
2025-03-22 03:39:24 +0400

    Deleted unused things

--- app/src/main/java/az/lahza/qarghadili/ui/screens/GenerateCrowLanguageScreen.kt
+++ app/src/main/java/az/lahza/qarghadili/ui/screens/GenerateCrowLanguageScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
